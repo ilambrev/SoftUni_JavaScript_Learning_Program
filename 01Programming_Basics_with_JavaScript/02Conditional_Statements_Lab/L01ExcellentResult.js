71b543a6e74354d6a5ff4669027aac7711077324
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-01-17 10:35:52 +0200

    Add files via upload

---
+++ 01Programming_Basics_with_JavaScript/02Conditional_Statements_Lab/L01ExcellentResult.js
@@ -0,0 +1,12 @@
+function exelentResult(input) {
+    let mark = input[0];
+
+    if (mark >= 5.50) {
+        console.log("Excellent!")
+    }
+}
+
+// exelentResult(["6"]);
+// exelentResult(["5"]);
+// exelentResult(["5.50"]);
+// exelentResult(["5.49"]);
