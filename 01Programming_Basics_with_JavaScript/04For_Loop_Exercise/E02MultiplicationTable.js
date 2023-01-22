d6c995f0ebbfb7eb49cf583897187c34d71f88d8
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-01-22 13:05:53 +0200

    Add files via upload

---
+++ 01Programming_Basics_with_JavaScript/04For_Loop_Exercise/E02MultiplicationTable.js
@@ -0,0 +1,12 @@
+function multiplicationTable(input) {
+
+    n = Number(input[0]);
+
+    for (let i = 1; i <= 10; i++) {
+        let result = i * n;
+        console.log(`${i} * ${n} = ${result}`);
+    }
+
+}
+
+// multiplicationTable(["5"]);
