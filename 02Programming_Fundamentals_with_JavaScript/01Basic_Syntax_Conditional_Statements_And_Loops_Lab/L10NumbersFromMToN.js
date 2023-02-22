51a1dbba2d047cb46809aef2c6cb1eef69686c78
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-02-22 18:23:43 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/01Basic_Syntax_Conditional_Statements_And_Loops_Lab/L10NumbersFromMToN.js
@@ -0,0 +1,16 @@
+function numbersFromMToN(m, n) {
+
+    let i = m;
+
+    while (i >= n) {
+
+        console.log(i);
+
+        i--;
+
+    }
+
+}
+
+// numbersFromMToN(6, 2);
+// numbersFromMToN(4, 1);
