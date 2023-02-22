0b0b8f14aa09420ee142854eaf7b0d6815bac0e4
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-02-22 18:20:59 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/01Basic_Syntax_Conditional_Statements_And_Loops_Lab/L02StudentInformation.js
@@ -0,0 +1,9 @@
+function studentInformation(studentName, studentAge, studentAverageGrade) {
+
+    console.log(`Name: ${studentName}, Age: ${studentAge}, Grade: ${studentAverageGrade.toFixed(2)}`);
+
+}
+
+// studentInformation('John', 15, 5.54678);
+// studentInformation('Steve', 16, 2.1426);
+// studentInformation('Marry', 12, 6.00);
