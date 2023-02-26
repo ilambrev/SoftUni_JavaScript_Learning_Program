e1dacd4f74cff1663e49f233f49b5ca62293ea51
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-02-26 17:25:47 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/02Data_Types_And_Variables_Lab/L07RequiredReading.js
@@ -0,0 +1,10 @@
+function requiredReading(bookPages, pagesPerHour, daysForReading) {
+
+    let numberOfHoursPerDay = (bookPages / pagesPerHour) / daysForReading;
+
+    console.log(numberOfHoursPerDay);
+
+}
+
+// requiredReading(212, 20, 2);
+// requiredReading(432, 15, 4);
