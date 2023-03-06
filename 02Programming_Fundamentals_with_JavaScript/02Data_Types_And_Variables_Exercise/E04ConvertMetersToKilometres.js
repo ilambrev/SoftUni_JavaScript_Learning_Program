f31d116bdbe73bcb3c52d45e953ed3a0a815c955
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-03-06 08:19:17 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/02Data_Types_And_Variables_Exercise/E04ConvertMetersToKilometres.js
@@ -0,0 +1,10 @@
+function convertMetersToKilometers(distanceInMeters) {
+
+    let distanceInKilometers = distanceInMeters / 1000;
+
+    console.log(distanceInKilometers.toFixed(2));
+
+}
+
+// convertMetersToKilometers(1852);
+// convertMetersToKilometers(798);
