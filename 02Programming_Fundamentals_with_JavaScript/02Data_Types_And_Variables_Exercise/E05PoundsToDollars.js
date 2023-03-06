75f4e92d59260a47e36c50b177fd34c403cad706
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-03-06 08:19:47 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/02Data_Types_And_Variables_Exercise/E05PoundsToDollars.js
@@ -0,0 +1,12 @@
+function poundsToDollars(pounds) {
+
+    const exchangeRate = 1.31;
+
+    let dollars = pounds * exchangeRate;
+
+    console.log(dollars.toFixed(3));
+
+}
+
+// poundsToDollars(80);
+// poundsToDollars(39);
