25dfd70efe0f2ffe3fc86d325cf756073ef808b2
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-03-06 08:16:21 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/02Data_Types_And_Variables_Exercise/E02CharsToString.js
@@ -0,0 +1,11 @@
+function charsToString(char1, char2, char3) {
+
+    let charsAsString = char1 + char2 + char3;
+
+    console.log(charsAsString);
+
+}
+
+// charsToString('a', 'b', 'c');
+// charsToString('%', '2', 'o');
+// charsToString('1', '5', 'p');
