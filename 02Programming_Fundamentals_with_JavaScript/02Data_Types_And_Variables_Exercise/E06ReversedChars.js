22f5f35da1340326aba46a8ee0f9f3e1eee346e2
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-03-06 08:20:15 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/02Data_Types_And_Variables_Exercise/E06ReversedChars.js
@@ -0,0 +1,10 @@
+function reversedChars(char1, char2, char3) {
+
+    let charsAsStringRevercedOrder = char3 + ' ' + char2 + ' ' + char1;
+
+    console.log(charsAsStringRevercedOrder);
+
+}
+
+// reversedChars('A', 'B', 'C');
+// reversedChars('1', 'L', '&');
