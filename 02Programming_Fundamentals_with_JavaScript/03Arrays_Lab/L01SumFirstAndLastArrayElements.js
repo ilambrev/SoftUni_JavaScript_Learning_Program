7581afff3c9883af0693957cbf2e5f39f5ac19ef
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-03-09 08:05:22 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/03Arrays_Lab/L01SumFirstAndLastArrayElements.js
@@ -0,0 +1,12 @@
+function sumFirstAndLastArrayElements(input) {
+
+    let firstElement = input[0];
+    let lastElement = input[input.length - 1];
+
+    console.log(firstElement + lastElement);
+
+}
+
+// sumFirstAndLastArrayElements([20, 30, 40]);
+// sumFirstAndLastArrayElements([10, 17, 22, 33]);
+// sumFirstAndLastArrayElements([11, 58, 69]);
