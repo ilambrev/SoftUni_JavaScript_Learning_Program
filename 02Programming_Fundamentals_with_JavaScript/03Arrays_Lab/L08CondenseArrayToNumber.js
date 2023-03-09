ac3812e5fe39236c74d8c6d745b671affb826cc4
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-03-09 10:09:02 +0200

    Add files via upload

---
+++ 02Programming_Fundamentals_with_JavaScript/03Arrays_Lab/L08CondenseArrayToNumber.js
@@ -0,0 +1,25 @@
+function condenseArrayToNumber(arr) {
+
+    let condensed = [...arr];
+
+    while (condensed.length > 1) {
+
+        let bufferArr = new Array(condensed.length - 1);
+
+        for (let i = 0; i < condensed.length - 1; i++) {
+
+            bufferArr[i] = condensed[i] + condensed[i + 1];
+
+        }
+
+        condensed = bufferArr;
+
+    }
+
+    console.log(condensed[0]);
+
+}
+
+// condenseArrayToNumber([2, 10, 3]);
+// condenseArrayToNumber([5, 0, 4, 1, 2]);
+// condenseArrayToNumber([1]);
