3c4add586ca419dd410ef3fbdb1e6788b27700f8
ilambrev <ilambrev@abv.bg>
2023-03-19 17:57:56 +0200

    L01FormatGrade

---
+++ 02Programming_Fundamentals_with_JavaScript/04Functions_Lab/L01FormatGrade.js
@@ -0,0 +1,35 @@
+function formatGrade(grade) {
+
+    let output = '';
+
+    if (grade < 3.00) {
+
+        output = 'Fail';
+
+    } else if (grade < 3.50) {
+
+        output = 'Poor';
+
+    } else if (grade < 4.50) {
+
+        output = 'Good';
+
+    } else if (grade < 5.50) {
+
+        output = 'Very good';
+
+    } else {
+
+        output = 'Excellent';
+
+    }
+
+    output = grade < 3.00 ? output + ' (2)' : output + ` (${grade.toFixed(2)})`;
+
+    console.log(output);
+
+}
+
+formatGrade(3.33);
+formatGrade(4.50);
+formatGrade(2.99);
