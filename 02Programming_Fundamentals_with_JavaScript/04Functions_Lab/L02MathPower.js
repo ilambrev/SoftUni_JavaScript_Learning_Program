dfabcfef072504a55920d627d7e32c2d709fcc49
ilambrev <ilambrev@abv.bg>
2023-03-19 17:58:40 +0200

    L02MathPower

---
+++ 02Programming_Fundamentals_with_JavaScript/04Functions_Lab/L02MathPower.js
@@ -0,0 +1,14 @@
+function mathPower(number, power) {
+
+    result = 1;
+
+    for (let i = 1; i <= power; i++) {
+        result *= number;
+    }
+
+    console.log(result);
+
+}
+
+mathPower(2, 8);
+mathPower(3, 4);
