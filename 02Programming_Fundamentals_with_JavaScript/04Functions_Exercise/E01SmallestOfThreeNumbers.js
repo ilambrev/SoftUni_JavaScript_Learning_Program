e79d955ea519600cd9816291812d2964dd0be67b
ilambrev <ilambrev@abv.bg>
2023-03-21 08:46:40 +0200

    E01SmallestOfThreeNumbers

---
+++ 02Programming_Fundamentals_with_JavaScript/04Functions_Exercise/E01SmallestOfThreeNumbers.js
@@ -0,0 +1,10 @@
+function smallestOfThreeNumbers(numOne, numTwo, numThree) {
+
+    console.log(Math.min(numOne, numTwo, numThree));
+
+}
+
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4);
+smallestOfThreeNumbers(2, 2, 2);
