5f77c425a30572061aad94793567c242d9901da5
ilambrev <ilambrev@abv.bg>
2023-03-25 16:16:14 +0200

    Sum First And Last

---
+++ 02Programming_Fundamentals_with_JavaScript/05Arrays_Advanced_Lab/L01SumFirstAndLast.js
@@ -0,0 +1,13 @@
+function sumFirstAndLast(input) {
+
+    let firsElement = Number(input[0]);
+    let lastElement = Number(input[input.length - 1]);
+
+    let sum = firsElement + lastElement;
+
+    console.log(sum);
+
+}
+
+// sumFirstAndLast(['20', '30', '40']);
+// sumFirstAndLast(['5', '10']);
