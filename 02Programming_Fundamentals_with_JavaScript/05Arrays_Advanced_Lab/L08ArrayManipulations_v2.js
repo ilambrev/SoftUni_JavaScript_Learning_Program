6a900f22dd5754a95589a7572ae2491cc291cca9
ilambrev <ilambrev@abv.bg>
2023-03-26 19:10:02 +0300

    Array Manipulations v2

--- 02Programming_Fundamentals_with_JavaScript/05Arrays_Advanced_Lab/L08ArrayManipulations_v2.js
+++ 02Programming_Fundamentals_with_JavaScript/05Arrays_Advanced_Lab/L08ArrayManipulations_v2.js
@@ -46,5 +46,5 @@ function arrayManipulations(input) {
 
 }
 
-arrayManipulations(['4 19 2 53 6 43', 'Add 3', 'Remove 2', 'RemoveAt 1', 'Insert 8 3']);
-arrayManipulations(['6 12 2 65 6 42', 'Add 8', 'Remove 12', 'RemoveAt 3', 'Insert 6 2']);
+// arrayManipulations(['4 19 2 53 6 43', 'Add 3', 'Remove 2', 'RemoveAt 1', 'Insert 8 3']);
+// arrayManipulations(['6 12 2 65 6 42', 'Add 8', 'Remove 12', 'RemoveAt 3', 'Insert 6 2']);
