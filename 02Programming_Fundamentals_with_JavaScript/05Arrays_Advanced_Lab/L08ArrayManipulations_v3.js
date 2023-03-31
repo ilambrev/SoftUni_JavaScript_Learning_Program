7f4144c513f5f56bda48a5a0029e9d46c01e795a
ilambrev <ilambrev@abv.bg>
2023-03-31 20:51:13 +0300

    L08ArrayManipulations_v3

---
+++ 02Programming_Fundamentals_with_JavaScript/05Arrays_Advanced_Lab/L08ArrayManipulations_v3.js
@@ -0,0 +1,52 @@
+function arrayManipulations(input) {
+
+    let numbersArr = input[0].split(' ').map(element => {
+        return Number(element);
+    });
+    let commandsArr = input.slice(1);
+
+    for (let command of commandsArr) {
+
+        let commandParams = command.split(' ');
+        let commandName = commandParams[0];
+
+        switch (commandName) {
+
+            case 'Add':
+
+                numbersArr.push(Number(commandParams[1]));
+
+                break;
+            case 'Remove':
+
+                numbersArr = numbersArr.filter(function (x) {
+                    return x !== Number(commandParams[1]);
+                });
+
+                break;
+            case 'RemoveAt':
+
+                let indexToRemove = Number(commandParams[1]);
+
+                numbersArr.splice(indexToRemove, 1);
+
+                break;
+            case 'Insert':
+
+                let numberToAdd = Number(commandParams[1]);
+                let indexToAdd = Number(commandParams[2]);
+
+                numbersArr.splice(indexToAdd, 0, numberToAdd);
+
+                break;
+
+        }
+
+    }
+
+    console.log(numbersArr.join(' '));
+
+}
+
+// arrayManipulations(['4 19 2 53 6 43', 'Add 3', 'Remove 2', 'RemoveAt 1', 'Insert 8 3']);
+// arrayManipulations(['6 12 2 65 6 42', 'Add 8', 'Remove 12', 'RemoveAt 3', 'Insert 6 2']);
