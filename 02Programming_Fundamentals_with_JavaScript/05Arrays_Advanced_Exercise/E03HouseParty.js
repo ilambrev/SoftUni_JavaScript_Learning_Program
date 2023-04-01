b9006eeb7a3d9c85aeba87910d4e648df3f69c8e
ilambrev <ilambrev@abv.bg>
2023-04-01 18:57:51 +0300

    House Party

--- 02Programming_Fundamentals_with_JavaScript/05Arrays_Advanced_Exercise/E03HouseParty.js
+++ 02Programming_Fundamentals_with_JavaScript/05Arrays_Advanced_Exercise/E03HouseParty.js
@@ -45,5 +45,5 @@ function houseParty(input) {
 
 }
 
-houseParty(['Allie is going!', 'George is going!', 'John is not going!', 'George is not going!']);
-houseParty(['Tom is going!', 'Annie is going!', 'Tom is going!', 'Garry is going!', 'Jerry is going!']);
+// houseParty(['Allie is going!', 'George is going!', 'John is not going!', 'George is not going!']);
+// houseParty(['Tom is going!', 'Annie is going!', 'Tom is going!', 'Garry is going!', 'Jerry is going!']);
