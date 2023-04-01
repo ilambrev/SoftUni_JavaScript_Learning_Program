aece137474e29faccd9c84b5db28020fc6813c76
ilambrev <ilambrev@abv.bg>
2023-04-01 18:55:49 +0300

    House Party

---
+++ 02Programming_Fundamentals_with_JavaScript/05Arrays_Advanced_Exercise/E03HouseParty.js
@@ -0,0 +1,49 @@
+function houseParty(input) {
+
+    let partyList = [];
+
+    for (let personData of input) {
+
+        let details = personData.split(' ');
+        let personName = details[0];
+
+        if (details.includes('not')) {
+
+            if (partyList.includes(personName)) {
+
+                let nameIndex = partyList.indexOf(personName);
+
+                if (nameIndex > -1) {
+
+                    partyList.splice(nameIndex, 1);
+
+                }
+
+            } else {
+
+                console.log(`${personName} is not in the list!`);
+
+            }
+
+        } else {
+
+            if (partyList.includes(personName)) {
+
+                console.log(`${personName} is already in the list!`);
+
+            } else {
+
+                partyList.push(personName);
+
+            }
+
+        }
+
+    }
+
+    console.log(partyList.join('\n'));
+
+}
+
+houseParty(['Allie is going!', 'George is going!', 'John is not going!', 'George is not going!']);
+houseParty(['Tom is going!', 'Annie is going!', 'Tom is going!', 'Garry is going!', 'Jerry is going!']);
