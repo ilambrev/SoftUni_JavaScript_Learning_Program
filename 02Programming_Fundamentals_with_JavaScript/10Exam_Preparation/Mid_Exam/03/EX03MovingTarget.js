a9d417031fe9beb75f87427e4d3f362d042fbc9b
ilambrev <ilambrev@abv.bg>
2023-04-07 22:04:53 +0300

    Moving Target

--- 02Programming_Fundamentals_with_JavaScript/10Exam_Preparation/Mid_Exam/03/EX03MovingTarget.js
+++ 02Programming_Fundamentals_with_JavaScript/10Exam_Preparation/Mid_Exam/03/EX03MovingTarget.js
@@ -81,5 +81,5 @@ function movingTarget(input) {
 
 }
 
-movingTarget(["52 74 23 44 96 110", "Shoot 5 10", "Shoot 1 80", "Strike 2 1", "Add 22 3", "End"]);
-movingTarget(["1 2 3 4 5", "Strike 0 1", "End"]);
+// movingTarget(["52 74 23 44 96 110", "Shoot 5 10", "Shoot 1 80", "Strike 2 1", "Add 22 3", "End"]);
+// movingTarget(["1 2 3 4 5", "Strike 0 1", "End"]);
