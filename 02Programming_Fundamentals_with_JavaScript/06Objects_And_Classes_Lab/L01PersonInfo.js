bf984a25bd18f1ee7ce1fda0a05f88fd17d9ab10
ilambrev <ilambrev@abv.bg>
2023-04-17 20:36:12 +0300

    Person Info

---
+++ 02Programming_Fundamentals_with_JavaScript/06Objects_And_Classes_Lab/L01PersonInfo.js
@@ -0,0 +1,10 @@
+function personInfo(firstNameParam, lastNameParam, ageParam) {
+
+    let person = { firstName: firstNameParam, lastName: lastNameParam, age: ageParam };
+
+    return person;
+
+}
+
+personInfo("Peter", "Pan", "20");
+personInfo("George", "Smith", "18")
