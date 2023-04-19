8b286aff7e87cefdec04317233987919fcafc6c7
ilambrev <ilambrev@abv.bg>
2023-04-19 20:40:30 +0300

    Cats v2

---
+++ 02Programming_Fundamentals_with_JavaScript/06Objects_And_Classes_Lab/L05Cats_v2.js
@@ -0,0 +1,39 @@
+function cats(catsInfo) {
+
+    class Cat {
+
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+
+    }
+
+    let cats = [];
+
+    for (let catInfo of catsInfo) {
+
+        let catDetails = catInfo.split(' ');
+        let name = catDetails[0];
+        let age = Number(catDetails[1]);
+
+        let cat = new Cat(name, age);
+
+        cats.push(cat);
+
+    }
+
+    for (let cat of cats) {
+
+        cat.meow();
+
+    }
+
+}
+
+cats(['Mellow 2', 'Tom 5']);
+cats(['Candy 1', 'Poppy 3', 'Nyx 2']);
