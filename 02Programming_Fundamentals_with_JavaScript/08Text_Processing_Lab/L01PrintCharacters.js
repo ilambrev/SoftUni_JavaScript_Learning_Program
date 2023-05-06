0ddfa9d1c418a0fdd312d511369fb75379d6c991
ilambrev <ilambrev@abv.bg>
2023-05-06 16:59:00 +0300

    Print Characters

---
+++ 02Programming_Fundamentals_with_JavaScript/08Text_Processing_Lab/L01PrintCharacters.js
@@ -0,0 +1,12 @@
+function printCharacters(inputString) {
+
+    for (let i = 0; i < inputString.length; i++) {
+
+        console.log(inputString.charAt(i));
+
+    }
+
+}
+
+// printCharacters('AWord');
+// printCharacters('Sentence');
