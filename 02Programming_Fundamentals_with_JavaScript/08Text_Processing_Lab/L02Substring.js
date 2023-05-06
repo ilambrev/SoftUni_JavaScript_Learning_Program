4f58e9f27fcd5fed2b603bc63cdc4c1250faf6cc
ilambrev <ilambrev@abv.bg>
2023-05-06 17:00:46 +0300

    Substring

---
+++ 02Programming_Fundamentals_with_JavaScript/08Text_Processing_Lab/L02Substring.js
@@ -0,0 +1,10 @@
+function substring(inputString, startIndex, count) {
+
+    let resultString = inputString.substring(startIndex, startIndex + count);
+
+    console.log(resultString);
+
+}
+
+// substring('ASentence', 1, 8);
+// substring('SkipWord', 4, 7);
