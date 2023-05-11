aae31fc04309fa78f176aecd9c7e369afdd8ca21
ilambrev <ilambrev@abv.bg>
2023-05-11 22:15:59 +0300

    Pascal Case Splitter

---
+++ 02Programming_Fundamentals_with_JavaScript/08Text_Processing_Exercise/E06PascalCaseSplitter.js
@@ -0,0 +1,25 @@
+function pascalCaseSplitter(input) {
+
+    let words = [];
+    let index = 0;
+
+    for (let i = 1; i < input.length; i++) {
+
+        if (input.charCodeAt(i) >= 65 && input.charCodeAt(i) <= 90) {
+
+            words.push(input.substring(index, i));
+            index = i;
+
+        }
+
+    }
+
+    words.push(input.substring(index, input.length));
+
+    console.log(words.join(', '));
+
+}
+
+// pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
+// pascalCaseSplitter('HoldTheDoor');
+// pascalCaseSplitter('ThisIsSoAnnoyingToDo');
