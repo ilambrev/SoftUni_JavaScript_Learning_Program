fc989b0073cb5ca7e9a42f9883172424dca16064
ilambrev <ilambrev@abv.bg>
2023-08-16 12:56:24 +0300

    Math Operations

---
+++ 03JSAdvanced/01Syntax_Functions_And_Statements_Lab/L05MathOperations.js
@@ -0,0 +1,17 @@
+function mathOperations(firstNum, secondNum, operator) {
+
+    let operations = {
+        '+': () => firstNum + secondNum,
+        '-': () => firstNum - secondNum,
+        '*': () => firstNum * secondNum,
+        '/': () => firstNum / secondNum,
+        '%': () => firstNum % secondNum,
+        '**': () => firstNum ** secondNum,
+    }
+
+    console.log(operations[operator]());
+
+}
+
+mathOperations(5, 6, '+');
+mathOperations(3, 5.5, '*');
