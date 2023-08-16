5cc17a96ca89c44112f0149aa12e42d32c48d43d
ilambrev <ilambrev@abv.bg>
2023-08-16 12:12:38 +0300

    String Length

---
+++ 03JSAdvanced/01Syntax_Functions_And_Statements_Lab/L02StringLength.js
@@ -0,0 +1,11 @@
+function stringLength(...input) {
+
+    let totalLength = input.reduce((acc, curr) => acc += curr.length, 0);
+    let averageLength = Math.round(totalLength / input.length);
+
+    console.log(`${totalLength}\n${averageLength}`);
+
+}
+
+// stringLength('chocolate', 'ice cream', 'cake');
+// stringLength('pasta', '5', '22.3');
