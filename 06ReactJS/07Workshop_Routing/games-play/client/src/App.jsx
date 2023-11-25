64a64b17c7d4cf1de10db4e6fb4a1c3037e37e5d
ilambrev <ilambrev@abv.bg>
2023-11-25 21:06:40 +0200

    Added Header component and site images

--- 06ReactJS/07Workshop_Routing/games-play/client/src/App.jsx
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/App.jsx
@@ -1,12 +1,12 @@
-
+import Header from "./components/header/Header";
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <div id="box">
+            <Header />
+        </div>
+    );
 };
 
 export default App;
