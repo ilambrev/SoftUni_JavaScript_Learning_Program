6d3c3ebee0ec6458f104478323671b27000fb138
ilambrev <ilambrev@abv.bg>
2023-11-27 11:44:25 +0200

    Register component added

--- 06ReactJS/07Workshop_Routing/games-play/client/src/App.jsx
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import GameList from "./components/game-list/GameList";
 import GameCreate from "./components/game-create/GameCreate";
 import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
                 <Route path="/games/create" element={<GameCreate />} />
 
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
 
             </Routes>
 
