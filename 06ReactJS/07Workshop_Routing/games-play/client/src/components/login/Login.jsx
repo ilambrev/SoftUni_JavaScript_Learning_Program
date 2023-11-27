73bff48b3195493d7d1faf1ee5ae5766e61dc918
ilambrev <ilambrev@abv.bg>
2023-11-27 11:38:04 +0200

    Login component added

---
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/components/login/Login.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+export default function Login() {
+    return (
+
+        <section id="login-page" className="auth">
+            <form id="login">
+
+                <div className="container">
+                    <div className="brand-logo"></div>
+                    <h1>Login</h1>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+
+                    <label for="login-pass">Password:</label>
+                    <input type="password" id="login-password" name="password" />
+                    <input type="submit" class="btn submit" value="Login" />
+                    <p className="field">
+                        <span>If you don't have profile click <Link to="/register">here</Link></span>
+                    </p>
+                </div>
+            </form>
+        </section>
+
+    );
+
+};
