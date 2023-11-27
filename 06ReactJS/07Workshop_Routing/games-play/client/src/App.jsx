c08b0474e742368285481bc5224cd231b62d07fc
ilambrev <ilambrev@abv.bg>
2023-11-27 11:29:31 +0200

    GamesCreate component added

--- 06ReactJS/07Workshop_Routing/games-play/client/src/App.jsx
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import GameList from "./components/game-list/GameList";
+import GameCreate from "./components/game-create/GameCreate";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
 
                 <Route path="/" element={<Home />} />
                 <Route path="/games" element={<GameList />} />
+                <Route path="/games/create" element={<GameCreate />} />
 
             </Routes>
 
