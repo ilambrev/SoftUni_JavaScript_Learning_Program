d5d82b69a8d59cd3d8740f9ad8f0222855a23db3
ilambrev <ilambrev@abv.bg>
2023-11-28 16:21:53 +0200

    Added getAll service which fetches all games from DB

--- 06ReactJS/07Workshop_Routing/games-play/client/src/components/game-list/GameList.jsx
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/components/game-list/GameList.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import * as gameService from "../../services/gameService";
+
 export default function GameList() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => setGames(result));
+    }, []);
+
+    console.log(games);
+
     return (
 
         <section id="catalog-page">
--- 06ReactJS/07Workshop_Routing/games-play/client/src/services/gameService.js
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/services/gameService.js
@@ -1,7 +1,15 @@
-const BASE_URL = 'http://localhost:3030/jsonstore';
+import { request } from "../lib/request";
+
+const BASE_URL = 'http://localhost:3030/jsonstore/games';
+
+export const getAll = async () => {
+    const result = await request('GET', BASE_URL);
+
+    return Object.values(result);
+};
 
 export const create = async (gameData) => {
-    const response = await fetch(`${BASE_URL}/games`, {
+    const response = await fetch(BASE_URL, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
