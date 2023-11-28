2923e3e74549598a7a00696793a839fbc09fe64f
ilambrev <ilambrev@abv.bg>
2023-11-28 16:39:53 +0200

    GameListItem added and display all games functionality implemented

---
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/components/game-list/game-list-item/GameListItem.jsx
@@ -0,0 +1,19 @@
+export default function ({
+    title,
+    category,
+    imageUrl,
+}) {
+
+    return (
+
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={imageUrl} />
+                <h6>{category}</h6>
+                <h2>{title}</h2>
+                <a href="#" className="details-button">Details</a>
+            </div>
+        </div>
+
+    );
+};
