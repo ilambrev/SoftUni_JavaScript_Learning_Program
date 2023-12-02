73d601a63406f02701b401beffd4cb9d3e93aa90
ilambrev <ilambrev@abv.bg>
2023-12-02 11:13:58 +0200

    Login functionality wit custom useForm hook

--- 06ReactJS/07Workshop_Routing/games-play/client/src/components/login/Login.jsx
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/components/login/Login.jsx
@@ -1,19 +1,39 @@
+import { useForm } from "../../hooks/useForm";
+
 import { Link } from "react-router-dom";
 
 export default function Login() {
+    const { values, onChange, onSubmit } = useForm({
+        email: '',
+        password: '',
+    });
+
     return (
 
         <section id="login-page" className="auth">
-            <form id="login">
+            <form id="login" onSubmit={onSubmit}>
 
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Sokka@gmail.com"
+                        onChange={onChange}
+                        value={values.email}
+                    />
 
                     <label for="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
+                    <input
+                        type="password"
+                        id="login-password"
+                        name="password"
+                        onChange={onChange}
+                        value={values.password}
+                    />
                     <input type="submit" class="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don't have profile click <Link to="/register">here</Link></span>
---
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/hooks/useForm.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export function useForm(submitHandler, initialValues) {
+    const [values, setValues] = useState(initialValues);
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        submitHandler(values);
+    };
+
+    return {
+        values,
+        onChange,
+        onSubmit,
+    };
+
+};
