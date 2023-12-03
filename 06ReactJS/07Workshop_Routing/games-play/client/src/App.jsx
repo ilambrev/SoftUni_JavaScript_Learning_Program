fc65ff36de8bcc255f40f118d12686d72c5e8ca1
ilambrev <ilambrev@abv.bg>
2023-12-03 18:01:00 +0200

    Dynamic navigation in header

--- 06ReactJS/07Workshop_Routing/games-play/client/src/App.jsx
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/App.jsx
@@ -25,14 +25,21 @@ function App() {
         navigate(Path.Home);
     };
 
+    const values = {
+        loginSubmitHandler,
+        username: auth.username,
+        email: auth.email,
+        isAuthenticated: !!auth.username,
+    };
+
     return (
-        <AuthContext.Provider value={{loginSubmitHandler}}>
+        <AuthContext.Provider value={values}>
             <div id="box">
                 <Header />
 
                 <Routes>
 
-                    <Route path="/" element={<Home />} />
+                    <Route path={Path.Home} element={<Home />} />
                     <Route path="/games" element={<GameList />} />
                     <Route path="/games/create" element={<GameCreate />} />
 
--- 06ReactJS/07Workshop_Routing/games-play/client/src/components/header/Header.jsx
+++ 06ReactJS/07Workshop_Routing/games-play/client/src/components/header/Header.jsx
@@ -1,19 +1,32 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../../contexts/AuthContext";
 
 export default function Header() {
+    const {
+        isAuthenticated,
+        username,
+    } = useContext(AuthContext);
+
     return (
         <header>
             <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
                 <Link to="/games">All games</Link>
-                <div id="user">
-                    <Link to="/games/create">Create Game</Link>
-                    <Link to="/logout">Logout</Link>
-                </div>
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
+                {isAuthenticated && (
+                    <div id="user">
+                        <Link to="/games/create">Create Game</Link>
+                        <Link to="/logout">Logout</Link>
+                        <span>| Welcome: <span style={{ color: "yellow", fontWeight: "bold" }}>{username}</span></span>
+                    </div>
+                )}
+
+                {!isAuthenticated && (
+                    <div id="guest">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </div>
+                )}
             </nav>
         </header>
     );
