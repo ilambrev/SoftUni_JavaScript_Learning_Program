112868d14ad049cce5d44fd735f80d550de10e79
ilambrev <ilambrev@abv.bg>
2024-01-02 19:38:30 +0200

    Area And Volume Calculator

---
+++ 03JSAdvanced/06Advanced_Functions_Lab/L01AreaAndVolumeCalculator.js
@@ -0,0 +1,34 @@
+function area() {
+    return Math.abs(this.x * this.y);
+}
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+}
+
+function solve(area, vol, input) {
+    const figures = JSON.parse(input);
+
+    function calc(figure) {
+        const figureArea = Math.abs(area.call(figure));
+        const figureVolume = Math.abs(vol.call(figure));
+
+        return { area: figureArea, volume: figureVolume };
+    }
+
+    return figures.map(calc);
+}
+
+// console.log(JSON.stringify(solve(area, vol, `[
+//     {"x":"1","y":"2","z":"10"},
+//     {"x":"7","y":"7","z":"10"},
+//     {"x":"5","y":"2","z":"10"}
+// ]`), null, 2));
+
+// console.log(JSON.stringify(solve(area, vol, `[
+//     {"x":"10","y":"-22","z":"10"},
+//     {"x":"47","y":"7","z":"-5"},
+//     {"x":"55","y":"8","z":"0"},
+//     {"x":"100","y":"100","z":"100"},
+//     {"x":"55","y":"80","z":"250"}
+// ]`), null, 2));
