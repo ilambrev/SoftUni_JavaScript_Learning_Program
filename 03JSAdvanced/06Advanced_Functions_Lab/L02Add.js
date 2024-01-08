8fc7196abba1b1473f0f8091381ca0234c428698
ilambrev <ilambrev@abv.bg>
2024-01-08 15:26:06 +0200

    Add

---
+++ 03JSAdvanced/06Advanced_Functions_Lab/L02Add.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    function add(a, b) {
+        return a + b;
+    }
+
+    return add.bind(this, num);
+}
+
+// let add5 = solution(5);
+// console.log(add5(2));
+// console.log(add5(3));
+
+// let add7 = solution(7);
+// console.log(add7(2));
+// console.log(add7(3));
