f4cbf56f7680ac9b106f82343d3b695e6dbaeeb8
ilambrev <ilambrev@abv.bg>
2024-01-12 16:31:05 +0200

    Playing Cards

---
+++ 03JSAdvanced/07Unit_Testing_And_Error_Handling_Lab/L02PlayingCards.js
@@ -0,0 +1,29 @@
+function playingCards(face, suit) {
+    const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+    const suits = {
+        S: '\u2660',
+        H: '\u2665',
+        D: '\u2666',
+        C: '\u2663',
+    }
+
+    if (face !== face.toUpperCase() || suit !== suit.toUpperCase() || !faces.includes(face)) {
+        return 'Error';
+    }
+
+    return {
+        face,
+        suit: suits[suit],
+
+        toString() {
+            return this.face + this.suit;
+        },
+    }
+}
+
+// console.log(playingCards('A', 'S').toString());
+
+// console.log(playingCards('10', 'H').toString());
+
+// console.log(playingCards('1', 'C').toString());
