7a95909e85925d72069cc7a7367459202ccf8c0f
ilambrev <ilambrev@abv.bg>
2024-03-14 14:01:17 +0200

    Bus Stop

---
+++ 04JSApplications/02Asynchronous_Programming_Exercise/E01BusStop/app.js
@@ -0,0 +1,26 @@
+function getInfo() {
+    const stopId = document.querySelector('#stopId');
+    const stopNameContainer = document.querySelector('#stopName');
+    const busesList = document.querySelector('#buses');
+    const baseUrl = 'http://localhost:3030/jsonstore/bus/businfo/';
+
+    stopNameContainer.textContent = '';
+    busesList.textContent = '';
+
+    fetch(`${baseUrl}${stopId.value}`)
+        .then(res => res.json())
+        .then(busStop => {
+            stopNameContainer.textContent = busStop.name;
+
+            Object.entries(busStop.buses).forEach(([busId, time]) => {
+                const busListItem = document.createElement('li');
+                busListItem.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                busesList.appendChild(busListItem);
+            });
+
+            stopId.value = '';
+        })
+        .catch(err => {
+            stopNameContainer.textContent = 'Error'
+        });
+}
