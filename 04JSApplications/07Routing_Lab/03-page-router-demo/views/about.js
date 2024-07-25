4eacc0fcc28960b58a70f459b5679db2918d20e3
ilambrev <ilambrev@abv.bg>
2024-07-25 11:46:03 +0300

    PAGE Router demo

--- 04JSApplications/07Routing_Lab/03-page-router-demo/views/about.js
+++ 04JSApplications/07Routing_Lab/03-page-router-demo/views/about.js
@@ -3,7 +3,7 @@ import { render, html } from "../node_modules/lit-html/lit-html.js";
 const aboutTemplate = () => html`
     <h1>About</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eaque?</p>
-`
+`;
 
 const root = document.querySelector('#root');
 
--- 04JSApplications/07Routing_Lab/03-page-router-demo/views/home.js
+++ 04JSApplications/07Routing_Lab/03-page-router-demo/views/home.js
@@ -3,7 +3,7 @@ import { render, html } from "../node_modules/lit-html/lit-html.js";
 const homeTemplate = () => html`
     <h1>Home</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, eaque?</p>
-`
+`;
 
 const root = document.querySelector('#root');
 
