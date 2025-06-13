d876b187a9c6d4ce1c0d770d056d991a91770f1e
Ivan Lambrev <ilambrev@abv.bg>
2025-06-13 08:46:33 +0300

    Hospital

---
+++ 01Programming_Basics_with_JavaScript/08More_Exercises/04For_Loop/ME02Hospital.js
@@ -0,0 +1,26 @@
+function hospital(input) {
+    let doctors = 7;
+    let treatedPatients = 0;
+    let untreatedPatients = 0;
+
+    for (let i = 1; i < input.length; i++) {
+        if (i % 3 == 0 && untreatedPatients > treatedPatients) {
+            doctors++;
+        }
+
+        const patientsForCurrentDay = Number(input[i]);
+
+        if (patientsForCurrentDay > doctors) {
+            untreatedPatients += patientsForCurrentDay - doctors;
+            treatedPatients += doctors;
+        } else {
+            treatedPatients += patientsForCurrentDay;
+        }
+    }
+
+    console.log(`Treated patients: ${treatedPatients}.`);
+    console.log(`Untreated patients: ${untreatedPatients}.`);
+}
+
+// hospital(['4', '7', '27', '9', '1']);
+// hospital(['6', '25', '25', '25', '25', '25', '2']);
