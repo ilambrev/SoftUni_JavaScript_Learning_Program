9a7f0ce4fa1c501dc8d126ee650cb1b3139bde3f
Ivan Lambrev <ilambrev@abv.bg>
2025-06-22 15:50:43 +0300

    Triangle Area

---
+++ 01Programming_Basics_with_JavaScript/08More_Exercises/01First_Steps_In_Coding/ME02TriangleArea.js
@@ -0,0 +1,8 @@
+function triangleArea(a, h) {
+    console.log(((a * h) / 2).toFixed(2));
+}
+
+// triangleArea(20, 30);
+// triangleArea(15, 35);
+// triangleArea(7.75, 8.45);
+// triangleArea(1.23456, 4.56789);
