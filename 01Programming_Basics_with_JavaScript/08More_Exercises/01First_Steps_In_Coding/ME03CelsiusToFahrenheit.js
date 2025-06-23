e22ccd432ae64b01f4df77012040c6c199d1a107
Ivan Lambrev <ilambrev@abv.bg>
2025-06-23 08:59:31 +0300

    Celsius To Fahrenheit

---
+++ 01Programming_Basics_with_JavaScript/08More_Exercises/01First_Steps_In_Coding/ME03CelsiusToFahrenheit.js
@@ -0,0 +1,8 @@
+function celsiusToFahrenheit(celsiusDegrees) {
+    console.log(((celsiusDegrees * 9 / 5) + 32).toFixed(2));
+}
+
+// celsiusToFahrenheit(25);
+// celsiusToFahrenheit(0);
+// celsiusToFahrenheit(-5.5);
+// celsiusToFahrenheit(32.3);
