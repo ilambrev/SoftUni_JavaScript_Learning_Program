17a0d2ffa6180f37f69c1cdd058620272750729b
Ivan Lambrev <ilambrev@abv.bg>
2025-06-25 10:20:11 +0300

    Rectangle Of N x N Stars

---
+++ 01Programming_Basics_with_JavaScript/08More_Exercises/07Drawing_Figures_With_Loops/ME02RectangleOfNxNStars.js
@@ -0,0 +1,9 @@
+function rectangleOfNxNStars(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log('*'.repeat(n));
+    }
+}
+
+// rectangleOfNxNStars(2);
+// rectangleOfNxNStars(3);
+// rectangleOfNxNStars(4);
