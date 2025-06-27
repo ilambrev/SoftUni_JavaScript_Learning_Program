5f83b1ba03d0519d27bec31187831d6e048c144e
Ivan Lambrev <ilambrev@abv.bg>
2025-06-27 17:15:21 +0300

    Numbers From 1 To 10

---
+++ 01Programming_Basics_with_JavaScript/08More_Exercises/03Conditional_Statements_Advanced/ME09NumbersFrom1To10.js
@@ -0,0 +1,10 @@
+function numbersFrom1To10() {
+    let currentNum = 1;
+
+    while (currentNum <= 10) {
+        console.log(currentNum);
+        currentNum++;
+    }
+}
+
+// numbersFrom1To10();
