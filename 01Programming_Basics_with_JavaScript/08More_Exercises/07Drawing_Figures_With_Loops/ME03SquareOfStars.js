62b7049a2383333af34076309a605c69c3c3c883
Ivan Lambrev <ilambrev@abv.bg>
2025-06-28 11:23:42 +0300

    Square Of Stars

---
+++ 01Programming_Basics_with_JavaScript/08More_Exercises/07Drawing_Figures_With_Loops/ME03SquareOfStars.js
@@ -0,0 +1,9 @@
+function squareOfStars(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log('* '.repeat(n -1) + '*');
+    }
+}
+
+// squareOfStars(2);
+// squareOfStars(3);
+// squareOfStars(4);
