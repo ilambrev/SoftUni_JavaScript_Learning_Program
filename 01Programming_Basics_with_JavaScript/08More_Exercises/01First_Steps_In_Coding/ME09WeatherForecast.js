b65a957877d2458d1285f692c37895588a5ad657
Ivan Lambrev <ilambrev@abv.bg>
2025-07-14 16:47:58 +0300

    Weather Forecast

---
+++ 01Programming_Basics_with_JavaScript/08More_Exercises/01First_Steps_In_Coding/ME09WeatherForecast.js
@@ -0,0 +1,7 @@
+function weatherForecast(weather) {
+    console.log(weather == "sunny" ? "It's warm outside!" : "It's cold outside!");
+}
+
+// weatherForecast('sunny');
+// weatherForecast('cloudy');
+// weatherForecast('snowy');
