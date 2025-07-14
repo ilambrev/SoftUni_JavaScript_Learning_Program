24a67546791d820fa0266d35627b4f9f12427ea5
Ivan Lambrev <ilambrev@abv.bg>
2025-07-14 13:13:44 +0300

    Circle Area And Perimeter

---
+++ 01Programming_Basics_with_JavaScript/08More_Exercises/01First_Steps_In_Coding/ME08CircleAreaAndPerimeter.js
@@ -0,0 +1,9 @@
+function circleAreaAndPerimeter(r) {
+    const s = Math.PI * r * r;
+    const p = 2 * Math.PI * r;
+
+    console.log(`${s.toFixed(2)}\n${p.toFixed(2)}`);
+}
+
+// circleAreaAndPerimeter(3);
+// circleAreaAndPerimeter(4.5);
